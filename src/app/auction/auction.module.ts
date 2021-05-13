9c33624f36eda2b78be1884891488d117442aeb8
domco545 <43946320+domco545@users.noreply.github.com>
2021-05-13 20:39:16 +0200

    fixed missing ngxs import in module

--- src/app/auction/auction.module.ts
+++ src/app/auction/auction.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 
 import { AuctionRoutingModule } from './auction-routing.module';
 import { AuctionListComponent } from './auction-list/auction-list.component';
+import {AuctionState} from './state/auction.state';
+import {NgxsModule} from '@ngxs/store';
 
 
 @NgModule({
   declarations: [AuctionListComponent],
   imports: [
     CommonModule,
-    AuctionRoutingModule
+    AuctionRoutingModule,
+    NgxsModule.forFeature([AuctionState])
   ]
 })
 export class AuctionModule { }
