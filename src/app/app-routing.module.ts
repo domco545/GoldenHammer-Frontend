830b3b20035dbd817d3ac58b169a47307af55619
domco545 <43946320+domco545@users.noreply.github.com>
2021-05-13 17:33:34 +0200

    added new modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {IndexComponent} from './index/index.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/index', pathMatch: 'full'},
+  {path: 'index', component: IndexComponent},
+  { path: 'auction', loadChildren: () => import('./auction/auction.module').then(m => m.AuctionModule) },
+  { path: 'user', loadChildren: () => import('./user/user.module').then(m => m.UserModule) },
+  { path: '**', component: PageNotFoundComponent },
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ConfigService } from './config.service';
 import { HttpClientModule } from '@angular/common/http';
+import { IndexComponent } from './index/index.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, IndexComponent, PageNotFoundComponent],
   imports: [BrowserModule, AppRoutingModule, NgbModule, HttpClientModule],
   providers: [ConfigService],
   bootstrap: [AppComponent],
